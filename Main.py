4ed499302eb244d2f945920d07377ea748774bad
MandichRiccardo <ricky.mandich@gmail.com>
2024-03-11 15:40:32 +0100

    caricamento 2024 03 11 15:40

---
+++ Main.py
@@ -0,0 +1,33 @@
+#esercizio 1
+nome = "Riccardo"
+print(nome)
+#esercizio 2
+eta = 17
+print(eta)
+#esercizio 3
+pi = 3.14159
+print(pi)
+#esercizio 4
+lunghezza = 10
+lunghezza = 15
+print(lunghezza)
+#esercizio 5
+nome_completo = "Riccardo Mandich"
+print(nome_completo)
+#esercizio 6
+eta_futura = eta + 10
+print(eta_futura)
+#esercizio 7
+nome = "nome_1"
+cognome = "cognome_1"
+anno_di_nascita = 2002
+print(nome, cognome, anno_di_nascita)
+
+nome = "nome_2"
+cognome = "cognome_2"
+anno_di_nascita = 2000
+print(nome, cognome, anno_di_nascita)
+#esercizio 8
+anno_corrente = 2024
+eta_attuale = anno_corrente-anno_di_nascita
+print(eta_attuale)
